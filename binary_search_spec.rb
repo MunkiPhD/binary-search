223816af903e8e261c081aa821b5d92814366335
MunkiPhD <munkiphd@gmail.com>
2013-05-27 12:04:33 -0400

    tests for iterative

--- binary_search_spec.rb
+++ binary_search_spec.rb
@@ -44,4 +44,41 @@ describe BinarySearch do
 			expect(@bs.chop(8, array)).to eq -1 #    assert_equal(-1, chop(8, [1, 3, 5, 7]))
 		end
 	end
+
+	context 'iterative' do
+		it 'returns the correct value' do
+			# shouldn't write all the test cases in one test, but there's really no point to breaking it all up
+			#check for nulls
+			expect(@bs.search(nil, [1])).to eq -1
+			expect(@bs.search(3, nil)).to eq -1
+			
+			# and these were the test cases provided
+			expect(@bs.search(3, [])).to eq -1	# assert_equal(-1, chop(3, []))
+
+			expect(@bs.search(3, [1])).to eq -1 # assert_equal(-1, chop(3, [1]))
+			expect(@bs.search(1, [1])).to eq 0	# assert_equal(0,  chop(1, [1]))
+
+			array = [1, 3, 5]
+			#
+			expect(@bs.search(1, array)).to eq 0	#	assert_equal(0,  chop(1, [1, 3, 5]))
+			expect(@bs.search(3, array)).to eq 1	# assert_equal(1,  chop(3, [1, 3, 5]))
+			expect(@bs.search(5, array)).to eq 2	# assert_equal(2,  chop(5, [1, 3, 5]))
+			expect(@bs.search(0, array)).to eq -1 # assert_equal(-1, chop(0, [1, 3, 5]))
+			expect(@bs.search(2, array)).to eq -1 # assert_equal(-1, chop(2, [1, 3, 5]))
+			expect(@bs.search(4, array)).to eq -1 # assert_equal(-1, chop(4, [1, 3, 5]))
+			expect(@bs.search(6, array)).to eq -1 # assert_equal(-1, chop(6, [1, 3, 5]))
+
+			array = [1, 3, 5, 7]
+			#
+			expect(@bs.search(1, array)).to eq 0	#    assert_equal(0,  chop(1, [1, 3, 5, 7]))
+			expect(@bs.search(3, array)).to eq 1	#    assert_equal(1,  chop(3, [1, 3, 5, 7]))
+			expect(@bs.search(5, array)).to eq 2	#    assert_equal(2,  chop(5, [1, 3, 5, 7]))
+			expect(@bs.search(7, array)).to eq 3	#    assert_equal(3,  chop(7, [1, 3, 5, 7]))
+			expect(@bs.search(0, array)).to eq -1	#    assert_equal(-1, chop(0, [1, 3, 5, 7]))
+			expect(@bs.search(2, array)).to eq -1 #    assert_equal(-1, chop(2, [1, 3, 5, 7]))
+			expect(@bs.search(4, array)).to eq -1 #    assert_equal(-1, chop(4, [1, 3, 5, 7]))
+			expect(@bs.search(6, array)).to eq -1 #    assert_equal(-1, chop(6, [1, 3, 5, 7]))
+			expect(@bs.search(8, array)).to eq -1 #    assert_equal(-1, chop(8, [1, 3, 5, 7]))
+		end
+	end
 end
